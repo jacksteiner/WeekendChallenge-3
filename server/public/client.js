4d40acccad092bd9e2c671c951e6d8c225d2fb6b
Jack Steiner <steiner.jack01@gmail.com>
2022-08-14 16:10:27 -0500

    started getTask function

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,23 @@
 $(document).ready(onReady);
 
 function onReady () {
-    
+    getTasks
+}
+
+function getTasks () {
+    $.ajax({
+        type: 'GET',
+        url: 'tasks/'
+    }).then(function(response){
+        console.log(response);
+        $('#taskTableBody').empty();
+        for (let i = 0; i < response.length; i++) {
+            let tasks = response[i];
+            // APPEND TASK, MAKE TASK.ROUTER FIRST
+            $('#taskTableBody').append(`
+                <tr>
+                    <td>
+            `)
+        }
+    })
 }
